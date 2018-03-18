e3c4b0386f9416873017835a1895655ac237ba23
Ido Harel <idohr1993@gmail.com>
2018-03-18 16:04:08 +0200

    metod in Point2D

--- src/Primitives/Point2D.java
+++ src/Primitives/Point2D.java
@@ -1,17 +1,36 @@
 package Primitives;
 
 public class Point2D {
+
     protected Coordinate _x;
     protected Coordinate _y;
     // ***************** Constructors ********************** //
-    public Point2D();
-    public Point2D(Coordinate x, Coordinate y);
-    public Point2D(Point2D point2D);
+    public Point2D(){}
+    public Point2D(Coordinate x, Coordinate y)
+    {
+        _x = x;
+        _y = y;
+    }
+    public Point2D(Point2D point2D)
+    {
+        _x = point2D.getX();
+        _y = point2D.getY();
+    }
     // ***************** Getters/Setters ********************** //
-    public Coordinate getX();
-    public Coordinate getY();
-    public void setX(Coordinate _x);
-    public void setY(Coordinate _y);
+    public Coordinate getX() {
+        return _x;
+    }
+    public void setX(Coordinate _x) {
+        this._x = _x;
+    }
+
+    public Coordinate getY() {
+        return _y;
+    }
+    public void setY(Coordinate _y) {
+        this._y = _y;
+    }
+
     // ***************** Administration ******************** //
     public int compareTo(Point2D point2D);
 }
