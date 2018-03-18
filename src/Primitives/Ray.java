5f42535df9d18fc27bae16f67287f23fb92ff5cb
yohanan <yohananha@gmail.com>
2018-03-18 15:29:02 +0200

    packeges primitive

--- src/Primitives/Ray.java
+++ src/Primitives/Ray.java
@@ -1,4 +1,17 @@
 package Primitives;
 
 public class Ray {
+    // Point of origin
+    private Point3D _POO;
+    // Ray direction
+    private Vector _direction;
+    // ***************** Constructors ********************** //
+    public Ray();
+    public Ray(Ray ray);
+    public Ray(Point3D poo, Vector direction);
+    // ***************** Getters/Setters ********************** //
+    public void setPOO(Point3D _POO);
+    public void setDirection(Vector _direction);
+    public Vector getDirection();
+    public Point3D getPOO();
 }
