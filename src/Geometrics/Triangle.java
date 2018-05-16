4568feb6db14094bcb75da4b1ae6331fd2c54452
Ido Harel <idohr1993@gmail.com>
2018-05-16 18:36:20 +0300

    inter triangle

--- src/Geometrics/Triangle.java
+++ src/Geometrics/Triangle.java
@@ -1,7 +1,8 @@
 package Geometrics;
 
-import Primitives.*;
-import Elements.Camera;
+import Primitives.Point3D;
+import Primitives.Ray;
+import Primitives.Vector;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -78,8 +79,59 @@ public class Triangle extends Geometry {
 
     ;
 
-    public List<Point3D> FindIntersections(Ray ray)
+    public List<Point3D> FindIntersections(Ray ray) throws Exception
     {
+        List<Point3D> ListInter = new LinkedList<Point3D>();
+
+        //find P0
+        Point3D P0 = ray.getPOO();
+
+        //find the triangle's Normal
+        Vector N = this.getNormal(this.getP1());
+
+        //find Plane
+        Plane plane = new Plane(N, this.getP3());
+
+        //if the intersection between the ray and plane
+        List<Point3D> ListIntersectionPlane = plane.FindIntersections(ray);
+
+        if(ListIntersectionPlane.isEmpty())
+            return ListInter;
+
+        Point3D intersectionPlane = ListIntersectionPlane.get(0);
+
+        //defin Vector P_P0
+        Vector P_P0 = new Vector(P0, intersectionPlane);
+
+        //checking side 1-2
+        Vector V1_1 = new Vector(P0, this._p1);
+        Vector V2_1 = new Vector(P0, this._p2);
+        Vector N1 = V1_1.crossProduct(V2_1);
+        N1.normalize();
+        double S1 = -P_P0.dotProduct(N1);
+
+        //checking side 2-3
+        Vector V1_2 = new Vector(P0, this._p2);
+        Vector V2_2 = new Vector(P0, this._p3);
+        Vector N2 = V1_2.crossProduct(V2_2);
+        N2.normalize();
+        double S2 = -P_P0.dotProduct(N2);
+
+        //checking side 3-1
+        Vector V1_3 = new Vector(P0, this._p3);
+        Vector V2_3 = new Vector(P0, this._p1);
+        Vector N3 = V1_3.crossProduct(V2_3);
+        N3.normalize();
+        double S3 = -P_P0.dotProduct(N3);
+
+        if (((S1 > 0) && (S2 > 0) && (S3 > 0)) || ((S1 < 0) && (S2 < 0) && (S3 < 0)))
+            ListInter.add(intersectionPlane);
+
+
+        return ListInter;
+
+
+
 
     }
 
