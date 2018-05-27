63b02a246a2fca098b30649eab808b727cb3e487
yohanan <yohananha@gmail.com>
2018-05-27 17:01:17 +0300

    flag

--- src/Test/renderer/ImageWriterTest.java
+++ src/Test/renderer/ImageWriterTest.java
@@ -19,19 +19,19 @@ public class ImageWriterTest {
                 if (i % 25 == 0 || j % 25 == 0 || i == j || i == 499 || j == 499 || i == 500 - j)
                     imageWriter.writePixel(j, i, 0, 0, 0);  // Black
                 else
-                if(i >= 200 && i <= 300 && j >= 200 && j <= 300)
+                if(i >= 0 && i <= 166 && j >= 0 && j <= 500)
                     imageWriter.writePixel(j, i, 255, 0, 0); // Red
                 else
-                if(i >= 150 && i <= 350 && j >= 150 && j <= 350)
-                    imageWriter.writePixel(j, i, 0, 255, 0);  // Green
+                if(i >= 167 && i <= 332 && j >= 0 && j <= 500)
+                    imageWriter.writePixel(j, i, 255, 255, 255);  // Green
                 else
-                if(i >= 100 && i <= 400 && j >= 100 && j <= 400)
+                if(i >= 333 && i <= 500 && j >= 0 && j <= 500)
                     imageWriter.writePixel(j, i, 0, 0, 255); // Blue
-                else
+               /* else
                 if(i >= 50 && i <= 450 && j >= 50 && j <= 450)
                     imageWriter.writePixel(j, i, 255, 255, 0); // Yellow
                 else
-                    imageWriter.writePixel(j, i, rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)); // Random
+                    imageWriter.writePixel(j, i, rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)); // Random*/
             }
         }
 
